75dc18527e6f51f728734c90da7783bc972ec198
cjones0429 <cejones1997@aim.com>
2021-08-03 12:48:14 -0400

    added macroapp.py, config.py and init.py to set up database

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,6 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models, errors
 bootstrap = Bootstrap(app)
 
 if __name__ == '__main__':
