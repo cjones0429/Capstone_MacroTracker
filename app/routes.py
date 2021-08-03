2bf1504e34bfe6bf94726f261f3a374662e7b8d1
cjones0429 <cejones1997@aim.com>
2021-08-03 12:50:44 -0400

    added route for database api search

--- app/routes.py
+++ app/routes.py
@@ -106,3 +106,76 @@ def search(date=None, meal=None):
                                        food_list_clean=food_list_clean, form=form, recent_list=recent_list)
 
 
+@app.route('/food/<string:ndbno>', methods=['GET', 'POST'])
+@app.route('/food/<string:date>/<string:meal>/<string:ndbno>',
+           methods=['GET', 'POST'])
+@login_required
+def get_nutrition(ndbno, meal=None, date=datetime.now()):
+    form1 = AddToFoodLogForm()
+
+    search_url = "https://api.nal.usda.gov/ndb/nutrients/?format=json"
+    params = dict(
+        api_key="ozs0jISJX6KiGzDWdXI7h9hCFBwYvk3m11HKkKbe",
+        nutrients=["205", "204", "208", "203"],
+        ndbno=ndbno
+    )
+
+    resp = requests.get(url=search_url, params=params)
+
+    if "No food" in str(resp.json()):
+        flash("No foods found.")
+        return redirect(url_for('search'))
+    else:
+        food_name = resp.json()['report']['foods'][0]['name']
+        food_measure = resp.json()['report']['foods'][0]['measure']
+        food_cals = resp.json()['report']['foods'][0]['nutrients'][0]['value']
+        food_protein = resp.json(
+        )['report']['foods'][0]['nutrients'][1]['value']
+        food_fat = resp.json()['report']['foods'][0]['nutrients'][2]['value']
+        food_carbs = resp.json()['report']['foods'][0]['nutrients'][3]['value']
+
+    if request.method == 'GET':
+        return render_template('nutrition.html',
+                               meal=meal,
+                               date=date,
+                               food_name=food_name,
+                               food_measure=food_measure,
+                               food_cals=food_cals,
+                               food_protein=food_protein,
+                               food_fat=food_fat,
+                               food_carbs=food_carbs,
+                               ndbno=ndbno,
+                               form1=form1,
+                               )
+
+    if request.method == 'POST':
+        if meal is None:
+            meal_choice = form1.meal.data
+        else:
+            meal_choice = meal
+
+        try:
+            quant_choice = float(form1.quantity.data)
+        except:
+            flash("Please enter valid values.")
+            return redirect(url_for('get_nutrition', ndbno=ndbno,
+                                    meal=meal, date=date))
+        else:
+            if quant_choice > 10000 or meal_choice not in ("Breakfast", "Lunch", "Dinner", "Snacks"):
+                flash("Please enter valid values.")
+                return redirect(url_for('get_nutrition', ndbno=ndbno,
+                                        meal=meal, date=date))
+            else:
+                food = Food(food_name=food_name, count=quant_choice,
+                            kcal=quant_choice * float(food_cals),
+                            protein=quant_choice * float(food_protein),
+                            fat=quant_choice * float(food_fat),
+                            carbs=quant_choice * float(food_carbs),
+                            unit=food_measure, meal=meal_choice,
+                            date=date, ndbno=ndbno, user_id=current_user.get_id())
+                db.session.add(food)
+                db.session.commit()
+
+                return redirect(url_for('food_log', date_pick=date))
+
+
